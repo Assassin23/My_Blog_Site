fe2ca9e1e6a46307fd70828fab0a23616074e1e6
Aditya <aditya332233@gmail.com>
2023-06-05 00:56:35 +0530

    used environment variable for creds.

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gbhyvh2^w&5d0fmhp6$4&9dprthlfl)3(=4qdeai32wzs#)=11"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -126,8 +130,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Email Sever Configuration
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "aditya332233@gmail.com"
-EMAIL_HOST_PASSWORD = "ofxngsiicmeklsxd"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
